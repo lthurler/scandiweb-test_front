f3d826d6ac07bb464fce3c3992cf81ad486e8705
lthurler <leothurler@gmail.com>
2023-06-02 21:36:09 -0300

    env

--- src/env.js
+++ src/env.js
@@ -1,6 +1,5 @@
 export const process = {
     env: {
-        // URL_API: "http://localhost:4000/scandiweb_test/product/",
         URL_API: "https://scandweb-test.000webhostapp.com/product/",
     }
 }
