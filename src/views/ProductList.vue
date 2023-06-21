373f589541d7f47d6abcad501d3d0a6ba2749aa7
lthurler <leothurler@gmail.com>
2023-06-21 11:56:54 -0300

    https

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -3,7 +3,7 @@ import HeaderVue from '../components/HeaderVue.vue'
 import FooterVue from '../components/FooterVue.vue'
 // import { process } from '../env.js'
 
-const url = "http://scandweb-test.infinityfreeapp.com/product/"
+const url = "https://scandweb-test.infinityfreeapp.com/product/"
 
 export default {
   name: 'ProductList',
