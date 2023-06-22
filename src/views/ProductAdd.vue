8ab7ebcbe427fa642bd50c0f1e83d31097feb049
lthurler <leothurler@gmail.com>
2023-06-22 15:54:33 -0300

    webhost

--- src/views/ProductAdd.vue
+++ src/views/ProductAdd.vue
@@ -6,7 +6,8 @@ import { required, decimal, helpers, maxLength, integer } from '@vuelidate/valid
 import { reactive, computed } from 'vue'
 // import { process } from '../env.js'
 
-const url = "https://scandweb-test.infinityfreeapp.com/product/"
+const url = "https://scandweb-test.000webhostapp.com/product/"
+  
 // const url = "https://scandweb-test.000webhostapp.com/product/"
 var skus = []
 
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -3,7 +3,8 @@ import HeaderVue from '../components/HeaderVue.vue'
 import FooterVue from '../components/FooterVue.vue'
 // import { process } from '../env.js'
 
-const url = "https://scandweb-test.infinityfreeapp.com/product/"
+// const url = "https://scandweb-test.infinityfreeapp.com/product/"
+const url = "https://scandweb-test.000webhostapp.com/product/"
 
 export default {
   name: 'ProductList',
